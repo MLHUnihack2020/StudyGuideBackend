314a6b1e0b7b70dea1b2b893f7c5e21b9c1e7e17
Max Ratmeyer <17440617+MaxRatmeyer@users.noreply.github.com>
2020-11-20 18:54:05 -0500

    Google Cloud Natural Language Processing API Test

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const app = express();
 
+const language = require('@google-cloud/language');
+const client = new language.LanguageServiceClient();
+
 const PORT = process.env.PORT || 8080
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+app.get('/', async (req, res) => {
+  const text = 'Hello, world!';
+
+  const document = {
+    content: text,
+    type: 'PLAIN_TEXT',
+  };
+
+  const [result] = await client.analyzeSentiment({document: document});
+  const sentiment = result.documentSentiment;
+
+  res.send(`Text: ${text}` + `Sentiment score: ${sentiment.score}` + `Sentiment magnitude: ${sentiment.magnitude}`);
 });
 
 app.listen(PORT, () => {
