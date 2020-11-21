7396bace60e408789d01b976955af14c90a446cb
Akash <akashpl2002@outlook.com>
2020-11-21 03:09:41 -0500

    Working questions and answers. Need to add blanks to the questions.

--- index.js
+++ index.js
@@ -41,23 +41,40 @@ app.post('/', async (req, res) => {
         
           let key = entity.name;
 
+          let found = false;
           let index = text.indexOf(key);
+          
+          console.log(key);
+          while (true) {
+            if (index - 1 >= 0 && index + 1 < text.length) {
+              console.log(index);
+              console.log(text[index - 1] + " | text | " + text[index + key.length])
+              if (!isAlpha(text[index - 1]) && !isAlpha(text[index + key.length])) {
+                console.log("hihihi");
+                break;
+              } else {
+                index = text.indexOf(key, index + 1);
+              }
+            }
+          }
+
           let period = text.indexOf('.');
           let prevPeriod = period;
           let question = '';
+
+          console.log(period + ": " + index);
 
           if (period > index) {
             question = text.substring(0, period).trim();
           } else {
             while (period < index) {
-              console.log(period + " " + index);
               prevPeriod = period;
               period = text.indexOf('.', period + 1);
             }
             question = text.substring(prevPeriod, period).trim();
           }
 
-          console.log(key);         
+          console.log(question);         
           questionBank.push({[entity.name]: question});
 
           response['flashcards'].push({ question: question, answer: entity.name })
@@ -65,23 +82,16 @@ app.post('/', async (req, res) => {
     }
   });
 
-
-
   console.log(questionBank)
 
   res.send(JSON.stringify(response));
-
-
-  // console.log('Entities:');
-  // entities.forEach(entity => {
-  //   console.log(entity.name);
-  //   console.log(` - Type: ${entity.type}, Salience: ${entity.salience}`);
-  //   if (entity.metadata && entity.metadata.wikipedia_url) {
-  //     console.log(` - Wikipedia URL: ${entity.metadata.wikipedia_url}`);
-  //   }
-  // });
-
 });
+
+
+function isAlpha(s) {
+  let alphabet = "abcdefghijklmnopqrstuvwxyz";
+  return alphabet.indexOf(s) >= 0;
+}
 
 app.listen(PORT, () => {
   console.log('Listening on port ' + PORT + '.');
