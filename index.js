1e8908b1e8c6f949266e7f8458737f4e4d9dd43f
Akash <akashpl2002@outlook.com>
2020-11-20 20:46:39 -0500

    Updated code to show salience scores for words in the text

--- index.js
+++ index.js
@@ -7,17 +7,24 @@ const client = new language.LanguageServiceClient();
 const PORT = process.env.PORT || 8080
 
 app.get('/', async (req, res) => {
-  const text = 'Hello, world!';
+  const text = 'Ignorant saw her her drawings marriage laughter. Case oh an that or away sigh do here upon. Acuteness you exquisite ourselves now end forfeited. Enquire ye without it garrets up himself. Interest our nor received followed was. Cultivated an up solicitude mr unpleasant. Much did had call new drew that kept. Limits expect wonder law she. Now has you views woman noisy match money rooms. To up remark it eldest length oh passed. Off because yet mistake feeling has men. Consulted disposing to moonlight ye extremity. Engage piqued in on coming. To they four in love. Settling you has separate supplied bed. Concluded resembled suspected his resources curiosity joy. Led all cottage met enabled attempt through talking delight. Dare he feet my tell busy. Considered imprudence of he friendship boisterous.';
 
   const document = {
     content: text,
     type: 'PLAIN_TEXT',
   };
 
-  const [result] = await client.analyzeSentiment({document: document});
-  const sentiment = result.documentSentiment;
+  const [result] = await client.analyzeEntities({document});
+  const entities = result.entities;
 
-  res.send(`Text: ${text}` + `Sentiment score: ${sentiment.score}` + `Sentiment magnitude: ${sentiment.magnitude}`);
+  console.log('Entities:');
+  entities.forEach(entity => {
+    console.log(entity.name);
+    console.log(` - Type: ${entity.type}, Salience: ${entity.salience}`);
+    if (entity.metadata && entity.metadata.wikipedia_url) {
+      console.log(` - Wikipedia URL: ${entity.metadata.wikipedia_url}`);
+    }
+  });
 });
 
 app.listen(PORT, () => {
